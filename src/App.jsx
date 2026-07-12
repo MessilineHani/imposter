e5d957d4a0a1205abbde4fb8b157733a6eb15cc9
MessilineHani <messiliinehani@gmail.com>
2026-07-12 17:37:39 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-orange-500 text-center">
+      Tailwindcss is working
+    </h1>
+  )
+}
