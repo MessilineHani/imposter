a1e22ab1fcd5a62ed11497be9b0e3ec9a393e726
MessilineHani <messiliinehani@gmail.com>
2026-08-23 22:03:43 +0100

    Setup(): Bottom sheet logic & functionality - up to reivew later

--- src/hooks/useBottomSheet.jsx
+++ src/hooks/useBottomSheet.jsx
@@ -1,8 +1,36 @@
 import { useState } from "react";
-
-export function useBottomSheet(nature) {
-  console.log(nature);
-  const [isOpen, setIsOpen] = useState(false);
-  const ToggleBottomSheet = () => setIsOpen((preToggle) => !preToggle);
-  return { isOpen, ToggleBottomSheet };
+import BottomSheet from "../components/BottomSheet/index.jsx";
+export default function useBottomSheet() {
+  console.log("useBottomSheet hook called");
+  const [bottomSheet, setBottomSheet] = useState({
+    element: null,
+    nature: null,
+    isOpen: false,
+  });
+  const BottomSheetContent = (nature) => {
+    switch (nature) {
+      case "Players":
+        return <BottomSheet.Players />;
+      case "Imposters":
+        return <BottomSheet.Imposters />;
+      case "TimeLimit":
+        return <BottomSheet.TimeLimit />;
+      case "Categories":
+        return <BottomSheet.Categories />;
+      case "Settings":
+        return <BottomSheet.Settings />;
+      case "Help":
+        return <BottomSheet.Help />;
+      default:
+        return null;
+    }
+  };
+  const ToggleBottomSheet = (nature) =>
+    setBottomSheet((preToggle) => ({
+      ...preToggle,
+      element: BottomSheetContent(nature),
+      nature: nature,
+      isOpen: nature === preToggle.nature ? !preToggle.isOpen : true,
+    }));
+  return { bottomSheet, ToggleBottomSheet };
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,34 @@
-import { useContext, useRef } from "react";
-import { ThemeContext } from "../context/theme.context.jsx";
-import { GameContext } from "../context/game.context.jsx";
 import BottomSheet from "../components/BottomSheet/index.jsx";
-
+import useBottomSheet from "../hooks/useBottomSheet.jsx";
 export default function Home() {
-  // const { game, dispatch } = useContext(GameContext);
+  const { bottomSheet, ToggleBottomSheet } = useBottomSheet();
+  const bottomSheetElements = [
+    "Players",
+    "Imposters",
+    "TimeLimit",
+    "Categories",
+    "Settings",
+    "Help",
+  ];
+  const bottomSheetButtonsDOM = bottomSheetElements.map((element) => {
+    return (
+      <button
+        key={element}
+        className="bg-blue-500 m-1 cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => ToggleBottomSheet(element)}
+      >
+        {element}
+      </button>
+    );
+  });
 
   return (
     <>
       <h1 className="text-2xl font-normal text-center text-green-500">
         Welcome to the Home Page
       </h1>
-      <BottomSheet>
-        {/* <BottomSheet.Players />
-        <BottomSheet.Imposters />
-        <BottomSheet.TimeLimit /> */}
-        <BottomSheet.Categories />
-      </BottomSheet>
+      {bottomSheetButtonsDOM}
+      <BottomSheet>{bottomSheet.isOpen && bottomSheet.element}</BottomSheet>
     </>
   );
 }
