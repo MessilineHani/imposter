843a68ca925db2253472398eaf60cc0a22b9a6fe
chimo star <messiliinehani@gmail.com>
2026-08-27 17:44:20 +0100

    Fix(): Clearing the seperate theme logic, settings context now provide theme information

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import { ThemeContext } from "./context/theme.context";
-import { useContext } from "react";
 import Home from "./pages/Home";
 import Roles from "./pages/Roles";
 import Discussion from "./pages/Discussion";
 import useGame from "./hooks/useGame";
+import useSettings from "./hooks/useSettings";
 import Timer from "./components/Timer";
 import Reveal from "./pages/Reveal";
 function DefineAppDOM(phase) {
@@ -27,12 +26,12 @@ function DefineAppDOM(phase) {
   }
 }
 export default function App() {
-  const { theme } = useContext(ThemeContext);
   const { game } = useGame();
+  const { settings } = useSettings();
   const appDOM = DefineAppDOM(game.phase);
   return (
     <div
-      className={`min-h-screen flex items-center flex-col justify-center ${theme === "dark" ? "bg-gray-900" : "bg-gray-100"}`}
+      className={`min-h-screen flex items-center flex-col justify-center ${settings.theme === "dark" ? "bg-gray-900" : "bg-gray-100"}`}
     >
       {appDOM}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ThemeProvider from "./context/theme.context.jsx";
 import GameProvider from "./context/game.context.jsx";
 import SettingsProvider from "./context/settings.context.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <SettingsProvider>
-      <ThemeProvider>
-        <GameProvider>
-          <App />
-        </GameProvider>
-      </ThemeProvider>
+      <GameProvider>
+        <App />
+      </GameProvider>
     </SettingsProvider>
   </StrictMode>,
 );
